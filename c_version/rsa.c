b2fb5eda81bdf5849bfb73e8499f4372d891c8dd
kokis1 <reuben.stannah@icloud.com>
2026-04-25 13:43:41 +0100

    started the c implementation

--- c_version/rsa.c
+++ c_version/rsa.c
@@ -1,4 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <stdint.h>
 
+#define PRIME_LIST "output.txt"
+
+int get_random_bytes(void *buffer, size_t length){
+   FILE *fptr = fopen("/dev/urandom", "rb");
+   size_t read = fread(buffer, 1, length, fptr);
+   fclose(fptr);
+   return (read == length) ? 0 : -1;
+}
+
+typedef struct{
+   int n;
+   int value;
+} key;
+
+key *gen_priv_key(){
+   uint64_t random_num_1;
+   uint64_t random_num_2;
+
+   if (get_random_bytes(&random_num_1, sizeof(random_num_1)) != 0){
+      printf("Failed to read from /dev/urandom");
+      return 1;
+   }
+   if (get_random_bytes(&random_num_2, sizeof(random_num_2)) != 0){
+      printf("Failed to read from /dev/urandom");
+      return 1;
+   }
+   
+
+}
