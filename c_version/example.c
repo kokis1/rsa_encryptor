80513aa69699686353213df614bb97349f2a5ff6
kokis1 <reuben.stannah@icloud.com>
2026-05-05 16:11:52 +0100

    finished the c implementation of RSA, in my opinion the best

--- c_version/example.c
+++ c_version/example.c
@@ -4,19 +4,59 @@
 #include "rsa.h"
 
 int main(int argc, char** argv){
-   key* pub_key = make_key();
-   key* priv_key = make_key();
 
-   if(gen_key_pair(pub_key, priv_key) == 1){
+   key* alice_priv = make_key();
+   key* alice_pub = make_key();
+
+   key* bob_priv = make_key();
+   key* bob_pub = make_key();
+
+
+   // ensuring the keys are created properly
+   if(gen_key_pair(alice_pub, alice_priv) != 0){
+      return 1;
+   }
+   if(gen_key_pair(bob_pub, bob_priv) != 0){
       return 1;
    }
 
-   uint64_t m = atoi(argv[1]);
-   uint64_t encrypted = encrypt(m, pub_key);
-   uint64_t decrypted = decrypt(encrypted, priv_key);
-   printf("message: %llu, cyphertext: %llu, decrypted: %llu\n", m, encrypted, decrypted);
+   // Bob wants to send a message to Alice
+   uint64_t bob_message = 1234;
 
-   kill_key(pub_key);
-   kill_key(priv_key);
+   // First, Bob encrypts the message using Alice's public key
+   uint64_t bob_cyphertext = encrypt(bob_message, alice_pub);
+
+   // Then Alice decrypts the message using her private key
+   uint64_t alice_decrypted = decrypt(bob_cyphertext, alice_priv);
+
+   // Then Alice sends the message back to Bob to confirm she recieved the message
+   uint64_t re_encrypted = encrypt(alice_decrypted, bob_pub);
+
+   // Bob then checks that his message matches the one recieved by Alice
+   uint64_t re_decrypted = decrypt(re_encrypted, bob_priv);
+
+   printf("Original Message: %llu\n", bob_message);
+   printf("Encrypted using Alice's public key: %llu\n", bob_cyphertext);
+   printf("Alice recieved the message and decrypted it using her private key: %llu\n", alice_decrypted);
+   printf("To confirm she recieved the message she first re-encrypts the message with Bob's public key: %llu\n", re_encrypted);
+   printf("Bob then decrypts his message from Alice to confirm the channel is secure: %llu\n", re_decrypted);
+
+
+   if(re_decrypted == bob_message){
+      printf("Woohoo!! secure channel achieved!\n");
+   }
+   else{
+      printf("Bad channel :(\n");
+   }
+
+
+
+
+
+   // freeing all the keys
+   kill_key(alice_priv);
+   kill_key(alice_pub);
+   kill_key(bob_priv);
+   kill_key(bob_pub);
    return 0;
 }
